96468ead5a4c88db58275e809740aa0c38c34bac
git-palace <edeveloper4@gmail.com>
2018-11-14 02:45:24 -0800

    updates

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -34,28 +34,27 @@ $(document).ready(function() {
 
                 $(node).on("change", function(e) {
                     var max_subscriber_number = scheduler.serverList("classes")[0]["size"];
+                    var instructor_label = "";
                     var class_id = $(this).val()
 
                     scheduler.serverList("classes").forEach(function(class_obj) {
                         if (class_obj["key"] == class_id) {
                             max_subscriber_number = class_obj["size"];
+                            instructor_label = class_obj["instructor"]["label"];
 
                             return true;
                         }
                     });
 
-                    node.parentNode.lastChild.firstChild.lastChild.innerHTML = max_subscriber_number;
-                    scheduler.formSection("subscribers").setValue([]);
+                    node.parentNode.childNodes[3].firstChild.innerHTML = instructor_label; //OK
+                    node.parentNode.lastChild.firstChild.lastChild.innerHTML = max_subscriber_number; //OK
+                    scheduler.formSection("subscribers").setValue([]); //OK
                 });
 
             }
-
-            if (value)
-                $(node).prop("disabled", true);
-            else
-                $(node).prop("disabled", false);
-
-            $(node).trigger("change");
+	    
+            $(node).prop("disabled", scheduler.getState().new_event ? false : true);
+            $(node).trigger("change"); //why trigger change? => because instructor label, max number texts should be updated when popup dialog opens and class id is changed
         },
         get_value: function(node, ev) {
             return $(node).val();
@@ -64,7 +63,8 @@ $(document).ready(function() {
     };
 
     scheduler.form_blocks["type_subscribers"] = {
-        render: function(sns) {
+        render: function(sns) { //can we load by ajax the datasource options from the server? If we load here the valid subscribere we remove the ajax from below from set_value method.
+            debugger;
             var sub_list = sns.options,
                 template = "";
 
@@ -84,76 +84,83 @@ $(document).ready(function() {
                     $(node).val(value);
                 else
                     $(node).val(value.split(","));
-
             } else {
                 $(node).val([]);
             }
-
             var select2Attrs = {
                 maximumSelectionLength: scheduler.serverList("classes")[0]["size"],
                 matcher: function(params, data) {
                     if (params.term && $(data.element).attr("data-phone").indexOf(params.term) == -1 && $(data.element).text().toLowerCase().indexOf(params.term.toLowerCase()) == -1) {
                         return null;
                     }
-
                     return data;
                 }
             };
-
             scheduler.serverList("classes").forEach(function(class_obj) {
-                if (class_obj["key"] == ev.class) {
+                if (class_obj["key"] == ev.classid) {
                     select2Attrs["maximumSelectionLength"] = class_obj["size"];
 
                     return true;
                 }
             })
-
             $(node).select2(select2Attrs);
-
-
-            if (!$(node).hasClass("change_event_attached")) {
+            if (!$(node).hasClass("change_event_attached")) {//what is this condition? used for what? => to prevent duplicated event hooks
                 $(node).addClass("change_event_attached");
 
                 $(node)
                     .on("select2:select", function(e) {
-                        scheduler.serverList("classes").forEach(function(class_obj) {
-                            if (class_obj["key"] == scheduler.formSection("class").getValue()) {
-                                var subscription_ids = [];
-
-                                class_obj["subscriptions"].forEach(function(subscription_obj) {
-                                    subscription_ids.push(subscription_obj["id"]);
-                                })
-
-                                $.post(
+                                $.get(
                                     "/event/api.php", {
                                         "subscriberID": e.params.data.id,
-                                        "subscriptions": JSON.stringify(subscription_ids)
+                                        "classID": scheduler.formSection("classid").getValue()
                                     },
-                                    function(data) {
-                                        node.childNodes.forEach(function(c_node) {
+                                    function(data) {//can we prevent the eselect2 event? => https://select2.org/programmatic-control/events
+                                        debugger
+                                        node.childNodes.forEach(function(c_node) {//why iterate? performance affected
                                             if (c_node.value == e.params.data.id) {
                                                 $(c_node).prop("selected", JSON.parse(data));
 
                                                 return true;
                                             }
                                         });
+                                        if(JSON.parse(data)){
+                                            dhtmlx.alert(e.params.data.text + " Added!")
+                                        }else  dhtmlx.alert(e.params.data.text + " does not have valid subscription!")
 
                                         $(node).trigger("change.select2");
                                     }
                                 )
-
-                            }
-                        });
                     })
+		    /*.on("select2:unselect", function(e) { //this was added by me like a separate functionality, is the same like select2:select
+
+                        var event_id = scheduler.getState().lightbox_id
+                        $.get("/event/ischeckin", {
+                            "subscriberID": e.params.data.id,
+                            "eventid": event_id
+                        }, function(data) {//can we prevent the eselect2 event?
+                            node.childNodes.forEach(function(c_node) {
+                                if (c_node.value == e.params.data.id) {
+                                    $(c_node).prop("selected", JSON.parse(data));
+
+                                    return true;
+                                }
+                            });
+                            if(JSON.parse(data)){
+                                dhtmlx.alert(e.params.data.text + " subscriber is in the class!")
+                            }else  dhtmlx.alert(e.params.data.text + " deleted!")
+
+                            $(node).trigger("change.select2");
+                        })
+                    })*/
                     .on("change", function() {
-                        if ($(node).val())
-                            scheduler.formSection("class").node.parentNode.lastChild.firstChild.firstChild.innerHTML = $(node).val().length;
+                        if ($(node).val())//why do we need chnage event if we have sesect and unselect defined?
+                            scheduler.formSection("classid").node.parentNode.lastChild.firstChild.firstChild.innerHTML = $(node).val().length;
                         else
-                            scheduler.formSection("class").node.parentNode.lastChild.firstChild.firstChild.innerHTML = 0;
+                            scheduler.formSection("classid").node.parentNode.lastChild.firstChild.firstChild.innerHTML = 0;
                     });
             };
 
-            $(node).trigger("change");
+            $(node).trigger("change");//why do you trigger again change event?
         },
         get_value: function(node, ev) {
             return $(node).val().join(",");
@@ -161,13 +168,13 @@ $(document).ready(function() {
         focus: function(node) {}
     };
 
-    scheduler.locale.labels.section_class = "Class";
+    scheduler.locale.labels.section_classid = "Class";
     scheduler.locale.labels.section_instructor = "Instructor";
     scheduler.locale.labels.section_text = "Text";
     scheduler.locale.labels.section_subscribers = "Subscribers";
 
     scheduler.config.lightbox.sections = [
-        { name: "class", height: 75, type: "type_cls_ins", map_to: "class", options: scheduler.serverList("classes") },
+        { name: "classid", height: 75, type: "type_cls_ins", map_to: "classid", options: scheduler.serverList("classes") },
         { name: "instructor", height: 0, type: "select", map_to: "instructor", options: scheduler.serverList("instructors") },
         { name: "text", height: 200, type: "textarea", map_to: "text" },
         { name: "subscribers", height: 150, type: "type_subscribers", map_to: "subscribers", options: scheduler.serverList("subscribers") },
@@ -191,21 +198,13 @@ $(document).ready(function() {
     scheduler.createUnitsView({
         name: "unit",
         property: "instructor",
-        list: scheduler.serverList("instructors"),
+        list: scheduler.serverList("instructors")
     });
 
-    /*scheduler.attachEvent("onBeforeViewChange", function(old_mode, old_date, mode, date) {
-        if (old_date && (old_date.getFullYear() != date.getFullYear() || old_date.getMonth() != date.getMonth())) {
-            scheduler.load("/event/get_events.php?year=" + date.getFullYear() + "&month=" + (date.getMonth() + 1), "json");
-        }
-
-        return true;
-    });*/
-
-
     scheduler.init("scheduler_here", new Date(), "month");
+    scheduler.setLoadMode("month");
     scheduler.load("/event/get_events.php", "json");
-
+    
     var dp = new dataProcessor("/event/get_events.php");
 
     dp.init(scheduler);
---
+++ event/get_subscribers.php
@@ -0,0 +1,121 @@
+<?php
+echo '[{
+            "label": "Bica Valentin14",
+            "phone": "1234581384",
+            "value": 23
+        },
+        {
+            "label": "Alina Mihai10",
+            "phone": "8231236842",
+            "value": 19
+        },
+        {
+            "label": "Popescu Catalin11",
+            "phone": "8135961325",
+            "value": 20
+        },
+        {
+            "label": "Alina Mihai",
+            "phone": "9238181383",
+            "value": 5
+        },
+        {
+            "label": "Bica Valentin9",
+            "phone": "3568582868",
+            "value": 9
+        },
+        {
+            "label": "Popescu Catalin11",
+            "phone": "7523828354",
+            "value": 11
+        },
+        {
+            "label": "Alina Mihai10",
+            "phone": "8246238454",
+            "value": 10
+        },
+        {
+            "label": "Popa Cristina13",
+            "phone": "6642858359",
+            "value": 13
+        },
+        {
+            "label": "Popa Cristina8",
+            "phone": "2341883200",
+            "value": 8
+        },
+        {
+            "label": "Ioana Valentina7",
+            "phone": "1597535846",
+            "value": 16
+        },
+        {
+            "label": "Bica Valentin14",
+            "phone": "8361548386",
+            "value": 14
+        },
+        {
+            "label": "Alina Mihai15",
+            "phone": "3660522489",
+            "value": 24
+        },
+        {
+            "label": "Alina Mihai15",
+            "phone": "2358624486",
+            "value": 15
+        },
+        {
+            "label": "Popescu Catalin",
+            "phone": "3344522185",
+            "value": 1
+        },
+        {
+            "label": "Ioana Valentina",
+            "phone": "3682211154",
+            "value": 2
+        },
+        {
+            "label": "Bica Valentin",
+            "phone": "5577885542",
+            "value": 4
+        },
+        {
+            "label": "Bica Valentin9",
+            "phone": "9966842584",
+            "value": 18
+        },
+        {
+            "label": "Ioana Valentina12",
+            "phone": "1234581384",
+            "value": 21
+        },
+        {
+            "label": "Popa Cristina8",
+            "phone": "1234581384",
+            "value": 17
+        },
+        {
+            "label": "Popa Cristina13",
+            "phone": "1234581384",
+            "value": 22
+        },
+        {
+            "label": "Popescu Catalin6",
+            "phone": "1234581384",
+            "value": 6
+        },
+        {
+            "label": "Popa Cristina",
+            "phone": "1234581384",
+            "value": 3
+        },
+        {
+            "label": "Ioana Valentina7",
+            "phone": "1234581384",
+            "value": 7
+        },
+        {
+            "label": "Ioana Valentina12",
+            "phone": "1234581384",
+            "value": 12
+        }]';
